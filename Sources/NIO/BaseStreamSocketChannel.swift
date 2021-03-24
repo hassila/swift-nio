013e78ba35d89a39087253acf7528554790ffe67
Joakim Hassila <joj@mac.com>
2021-03-24 10:30:00 +0100

    s

--- Sources/NIO/BaseStreamSocketChannel.swift
+++ Sources/NIO/BaseStreamSocketChannel.swift
@@ -137,6 +137,7 @@ class BaseStreamSocketChannel<Socket: SocketProtocol>: BaseSocketChannel<Socket>
     }
 
     final override func writeToSocket() throws -> OverallWriteResult {
+        _debugPrint("writeToSocket fd[\(descriptor)]")
         let result = try self.pendingWrites.triggerAppropriateWriteOperations(scalarBufferWriteOperation: { ptr in
             guard ptr.count > 0 else {
                 // No need to call write if the buffer is empty.
