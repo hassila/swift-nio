27b7baf3682f73b53f68b65dbc4e7175b1ba2a2d
Joakim Hassila <joj@mac.com>
2021-03-24 17:47:42 +0100

    pdf

--- Sources/NIO/Linux.swift
+++ Sources/NIO/Linux.swift
@@ -151,8 +151,8 @@ enum CqeEventType : Int {
 
 public class Uring {
     private var ring = io_uring()
-    private let ring_entries: CUnsignedInt = 512 // 4096
-    private let cqeCount = 512
+    private let ring_entries: CUnsignedInt = 8192 // 4096
+    private let cqeCount = 4096
 
     // FIXME: cqeCount should be bumped to a larger value for deployment, just keeping small for debugging
     //    private let cqeCount = 10000
