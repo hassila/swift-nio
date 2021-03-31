572a0f9af8220ae56bb364146212fb9bdd473778
Joakim Hassila <joj@mac.com>
2021-03-31 15:33:59 +0200

    enable tests again

--- Tests/NIOTests/StreamChannelsTest+XCTest.swift
+++ Tests/NIOTests/StreamChannelsTest+XCTest.swift
@@ -31,7 +31,7 @@ extension StreamChannelTest {
                 ("testSyncChannelOptions", testSyncChannelOptions),
                 ("testChannelReturnsNilForDefaultSyncOptionsImplementation", testChannelReturnsNilForDefaultSyncOptionsImplementation),
                 ("testWritabilityStartsTrueGoesFalseAndBackToTrue", testWritabilityStartsTrueGoesFalseAndBackToTrue),
-// FIXME: SIGPIPE               ("testHalfCloseOwnOutput", testHalfCloseOwnOutput),
+                ("testHalfCloseOwnOutput", testHalfCloseOwnOutput),
                 ("testHalfCloseOwnInput", testHalfCloseOwnInput),
                 ("testDoubleShutdownInput", testDoubleShutdownInput),
                 ("testDoubleShutdownOutput", testDoubleShutdownOutput),
