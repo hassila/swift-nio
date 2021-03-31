6d2bed78f8a4324b685afc43924295fa759420aa
Joakim Hassila <joj@mac.com>
2021-03-31 08:23:56 +0200

    Fix build.

--- Sources/NIO/Selector.swift
+++ Sources/NIO/Selector.swift
@@ -1117,7 +1117,7 @@ final internal class UringSelector<R: Registration>: Selector<R> {
                     // FIXME: This is only needed due to the edge triggered nature of liburing, possibly
                     // we can get away with only updating (force triggering an event if available) for
                     // partial reads (where we currently give up after 4 iterations)
-                    if multishot && shouldRefreshPollForEvent(selectorEvent) { // can be after guard as it is multishot
+                    if multishot && shouldRefreshPollForEvent(selectorEvent:selectorEvent) { // can be after guard as it is multishot
                         ring.io_uring_poll_update(fd: event.fd, newPollmask: registration.interested.uringEventSet, oldPollmask:registration.interested.uringEventSet, submitNow:false)
                     }
 
