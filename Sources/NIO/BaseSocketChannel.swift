5190b6bd69bf3e7e35af62ae5f97f98b797e2345
Joakim Hassila <joj@mac.com>
2021-04-04 11:35:05 +0200

    xxx

--- Sources/NIO/BaseSocketChannel.swift
+++ Sources/NIO/BaseSocketChannel.swift
@@ -694,11 +694,11 @@ class BaseSocketChannel<SocketType: BaseSocketProtocol>: SelectableChannel, Chan
     func unregisterForWritable() {
         self.eventLoop.assertInEventLoop()
 print("AAA XXX")
-        guard self.interestedEvent.contains(.write) else {
-            print("AAA XXX ZZZZ")
+//        guard self.interestedEvent.contains(.write) else {
+//            print("AAA XXX ZZZZ")
             // nothing to do if we were not previously interested in write
-            return
-        }
+//            return
+//        }
         self.safeReregister(interested: self.interestedEvent.subtracting(.write))
     }
 
